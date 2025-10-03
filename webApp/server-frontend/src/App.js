7368176ad744d9cd27025d4ad1d337c725d9103f
Kilo27 <kyle.oseoighe@gmail.com>
2025-10-03 22:59:18 +0100

    iframe

--- webApp/server-frontend/src/App.js
+++ webApp/server-frontend/src/App.js
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+		<iframe src='' width="" height=""></iframe>
+	  </header>
     </div>
   );
 }
