c1acdd7d528cd6ae8a4caf9e2707f28222d8005a
Kilo27 <kyle.oseoighe@gmail.com>
2025-10-04 04:05:30 +0100

    this works

--- pythonBS/app.py
+++ pythonBS/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from datetime import datetime
-from src.getTrajectory import calculate_asteroid_trajectory
+from src import getTrajectory
 from API_calls import getData
 
 app=Flask(__name__)
@@ -30,8 +30,15 @@ def get_asteroids(methods="POST"):
 @app.route("/getOrbitalDataById")#http://localhost:5000/getOrbitalDataById?asteroid_id=2497232
 def get_orbital_data(methods="POST"):
     asteroid_id=int(request.args.get("asteroid_id"))
-    return jsonify(getData.orbitalData(asteroid_id))
+    return getData.orbitalData(asteroid_id)
 
+@app.route("/calculateAsteroidTrajectory")
+def get_trajectory():
+    asteroid_id=int(request.args.get("asteroid_id"))
+    elements = getData.orbitalData(asteroid_id)
+    end_date=request.args.get("end_date")
+    print(getTrajectory.calculate_asteroid_trajectory(elements, end_date))
+    return str(getTrajectory.calculate_asteroid_trajectory(elements, end_date))#http://localhost:5000/calculateAsteroidTrajectory?asteroid_id=2497232&end_date=2026-10-01
 
 if __name__ == '__main__':
     app.run()
